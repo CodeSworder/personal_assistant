d795e68c5276eecc7594cd200c838fc2a8c114ae
CodeSword <1257226255@qq.com>
2024-03-13 11:49:02 +0800

    test

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
 import os
 os.system('pwd;ls')
-os.system('cd config;xtuner train ./config/internlm_chat_7b_qlora_oasst1_e3_copy.py')
-os.system("cd code;git clone https://gitee.com/internlm/InternLM.git;cd InternLM;git checkout 3028f07cb79e5b1d7342f4ad8d11efad3fd13d17")
+os.system('cd xtuner019/xtuner;pip install -e '.[all]';xtuner train /home/xlab-app-center/config/internlm_chat_7b_qlora_oasst1_e3_copy.py')
+os.system("mkdir code;cd code;git clone https://gitee.com/internlm/InternLM.git;cd InternLM;git checkout 3028f07cb79e5b1d7342f4ad8d11efad3fd13d17")
 # 定义要执行的所有命令
 commands = """
-mkdir ./config/work_dirs/hf;
+mkdir /home/xlab-app-center/config/work_dirs/hf;
 export MKL_SERVICE_FORCE_INTEL=1;
 export CONFIG_NAME_OR_PATH=./config/internlm_chat_7b_qlora_oasst1_e3_copy.py;
 export PTH=./config/work_dirs/internlm_chat_7b_qlora_oasst1_e3_copy/epoch_3.pth;
@@ -14,7 +14,7 @@ export MKL_SERVICE_FORCE_INTEL=1;
 export MKL_THREADING_LAYER='GNU';
 export NAME_OR_PATH_TO_LLM=Shanghai_AI_Laboratory/internlm-chat-7b;
 export NAME_OR_PATH_TO_ADAPTER=./config/work_dirs/hf;
-mkdir ./config/work_dirs/hf_merge;
+mkdir /home/xlab-app-center/config/work_dirs/hf_merge;
 export SAVE_PATH=./config/work_dirs/hf_merge;
 xtuner convert merge $NAME_OR_PATH_TO_LLM $NAME_OR_PATH_TO_ADAPTER $SAVE_PATH --max-shard-size 2GB;
 pip install streamlit==1.24.0
@@ -23,4 +23,4 @@ pip install streamlit==1.24.0
 # 通过os.system()执行所有命令
 os.system(commands)
 
-os.system('streamlit run ./code/InternLM/web_demo.py --server.address 127.0.0.1 --server.port 6006')
+os.system('streamlit run /home/xlab-app-center/code/InternLM/web_demo.py --server.address 127.0.0.1 --server.port 6006')
