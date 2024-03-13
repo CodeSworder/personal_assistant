7837fbfdb220948c99a2612bdd8186c9459ed7c6
CodeSword <1257226255@qq.com>
2024-03-13 10:10:06 +0800

    test

--- start.py
+++ start.py
@@ -23,4 +23,4 @@ pip install streamlit==1.24.0
 # 通过os.system()执行所有命令
 os.system(commands)
 
-os.system('streamlit run code/InternLM/web_demo.py --server.address 127.0.0.1 --server.port 6006')
+os.system('streamlit run ./code/InternLM/web_demo.py --server.address 127.0.0.1 --server.port 6006')
